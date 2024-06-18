c1822f5c33376b94914f8630f0340dc62bd11a99
mkvvmk25 <152393796+mkvvmk25@users.noreply.github.com>
2024-06-18 14:27:01 +0530

    Update gr.cpp

--- graph/gr.cpp
+++ graph/gr.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 using namespace std; 
 
 
@@ -14,7 +15,7 @@ class GraphUD
             vertex = n;
             edges = 0;
             adjlist = new vector<vector<int>>(vertex);
-            adjlist->push_back( vector<int>() )
+            adjlist->push_back( vector<int>() );
         
         }
 
@@ -24,6 +25,56 @@ class GraphUD
             adjlist->at(des).push_back(src); 
             edges += 2; 
         }
+        void solver(int stand, vector<int> &vis)
+        {
+            vis[stand] = 1; 
+            cout << stand << " "; 
+
+            // look
+            for (int j = 0; j < adjlist->at(stand).size(); j++)
+            {
+                int next = adjlist->at(stand).at(j);
+                if (vis[next] == -1)
+                {
+                    solver(next, vis); 
+                }
+            }
+        }
+        void dfs(int start)
+        {
+            vector<int> vis(vertex, -1); 
+            solver(start, vis);
+            //      3  
+
+        }
+        void bfs(int start)
+        {
+            queue<int> que; 
+            // int *vis  = new int[vertex]; 
+            vector<int> vis(vertex, -1); 
+            que.push(start); 
+            vis[start] = 1; 
+
+            while( que.empty() != true)
+            {
+                int stand = que.front();
+                que.pop(); 
+                cout << stand <<" "; 
+
+                // look at nei 
+                for(int j = 0; j < adjlist->at(stand).size(); j++)
+                {
+                    int next = adjlist->at(stand).at(j); 
+                    if( vis[next] == -1)
+                    {
+                        que.push(next);
+                        vis[next] = 1; 
+                    }
+                }
+
+            }
+
+        }
 
         void print( )
         {
@@ -32,7 +83,7 @@ class GraphUD
                 cout << i <<": "; 
                 for(int j = 0; j < adjlist->at(i).size(); j++ )
                 {
-                    cout << adjlist->at(i).at(j); 
+                    cout << adjlist->at(i).at(j) << " "; 
                 }
 
                 cout << endl;
@@ -45,7 +96,6 @@ int main()
 {
     GraphUD g = GraphUD(5); 
     g.addEdge(0,1); 
-    g.addEdge(0,1); 
     g.addEdge(0,2); 
     g.addEdge(3,4); 
     g.addEdge(2,1); 
@@ -53,9 +103,22 @@ int main()
     g.addEdge(2,4); 
 
 
-    vector<int> p; 
-    vector<int> *p1 = new vector<int>(5,0);  
-    p1->push_back(5); 
+    g.print();
+
+    /* vector<int> **p; 
+    p = new vector<int>*;
+    *(p + 0) = new vector<int>(2,20); */
+
+    int* p, a, c; 
+
+    int h = 45;
+    p = &h;
+    a = h;
+    c = h; 
+
+    cout << *p << endl;
+    cout << a << endl;
+    cout << c << endl;
 
     return 0; 
 }
